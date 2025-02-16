a756a30d30e2e7fe57193ff1ce4f7263ffcd2c68
Julian <julianthoms@gmail.com>
2025-02-16 08:37:59 +0100

    Initial commit.final

--- src/app/test-button/test-button.component.ts
+++ src/app/test-button/test-button.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-test-button',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './test-button.component.html',
   styleUrl: './test-button.component.scss'
 })
